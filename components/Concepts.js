9f23a29e74f38e0c075c2ce86cd5016f59b1a914
shaqdeff <shaquillendunda@gmail.com>
2023-12-14 04:27:00 +0300

    Update sections

--- components/Concepts.js
+++ components/Concepts.js
@@ -1,9 +1,9 @@
 /** @format */
 
-import Image from "next/image";
-import React from "react";
-import concept1 from "../images/concept1.png";
-import concept2 from "../images/concept2.png";
+import Image from 'next/image';
+import React from 'react';
+import concept1 from '../images/simz-twist.jpg';
+import concept2 from '../images/funk-avenue.webp';
 function Concepts() {
   return (
     <div className="relative font-neutralFace px-3 w-full h-fit">
--- components/Publications.js
+++ components/Publications.js
@@ -2,16 +2,28 @@
 
 import Image from 'next/image';
 import React from 'react';
-import publish from '../images/publications.png';
+import Link from 'next/link';
+import publish from '../images/ahora.jpg';
+
 function Publications() {
   return (
     <div id="publications-section" className="relative font-neutralFace">
       <div className="px-3">
         <div className="w-full h-[1px] bg-white"></div>
-        <div className="flex text-xl md:text-4xl my-3 font-bold justify-between mb-36 w-full">
-          <div>EPs</div>
-          <div>ALBUMS</div>
-          <div>ALL WORK</div>
+        <div className="flex text-base md:text-4xl my-3 font-bold justify-between mb-36 w-full">
+          <Link
+            href="https://songwhip.com/funkavenue/ahora-festejamos"
+            target="_blank">
+            AHORA FESTEJAMOS
+          </Link>
+          <Link
+            href="https://songwhip.com/funkavenue/now-we-break-bread"
+            target="_blank">
+            NOW WE BREAK BREAD
+          </Link>
+          <Link href="https://songwhip.com/funkavenue" target="_blank">
+            ALL WORK
+          </Link>
         </div>
       </div>
       <div className="bg-[#e7e7e7] pb-[20vh]">
@@ -22,7 +34,12 @@ function Publications() {
               <h1 className="text-4xl my-6 font-bold">
                 UNCOMPROMISED CREATIVITY
               </h1>
-              <p className="underline">EXPLORE PUBLICATION</p>
+              <Link
+                href="https://open.spotify.com/album/4MBZ0nQ5t3zYcTYf1CiuMu?si=3qCeD8DYTlOFznaAftYNhQ"
+                target="_blank"
+                className="underline">
+                EXPLORE PUBLICATION
+              </Link>
             </div>
             <div className="h-[10%] flex w-full justify-center items-center flex-col">
               <div className="w-full h-[2px] bg-black"></div>
@@ -30,7 +47,7 @@ function Publications() {
             </div>
           </div>
           <div className="w-full md:w-[50%] flex items-center justify-center bg-[#e7e7e7] md:bg-black h-full">
-            <Image alt="" src={publish} className="w-[70%]" />
+            <Image alt="" src={publish} className="w-[100%]" />
           </div>
         </div>
       </div>
